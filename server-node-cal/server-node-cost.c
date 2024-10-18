2614664f2df9902b8bb99c5cfdfaa52114ca9b26
dhruvchandak30 <dhruvchandak5@gmail.com>
2024-10-18 10:53:17 +0530

    Added function for 3 Nodes

--- server-node-cal/server-node-cost.c
+++ server-node-cal/server-node-cost.c
@@ -6,8 +6,14 @@ float costOfTwoNodes(float cost1, float cost2)
     return 1 + (cost2 * ((1.0 / cost1) * (1 - (1.0 / cost2)))) + (cost1 * ((1.0 / cost2) * (1 - (1.0 / cost1))));
 }
 
+float costofThreeNodes(float c1, float c2, float c3)
+{
+    return (c1 + c2 + c3 + (c1 * c2 * c3) - (2 * c1 * c2) - (2 * c2 * c3) - (2 * c3 * c1) + (c3 * costOfTwoNodes(c1, c2)) + (c1 * costOfTwoNodes(c2, c3)) + (c2 * costOfTwoNodes(c3, c1))) / ((1 - c1) * (1 - c2) * (1 - c3));
+}
 
-
+float costofFourNodes(float c1, float c2, float c3, float c4)
+{
+}
 
 int main()
 {
@@ -24,8 +30,25 @@ int main()
         scanf("%f", &val);
         cost[j] = val;
     }
+    float ans = 0;
+    if (n == 2)
+    {
 
-    float ans = costOfTwoNodes(cost[0], cost[1]);
+        ans = costOfTwoNodes(cost[0], cost[1]);
+    }
+    else if (n == 3)
+    {
+        ans = costofThreeNodes(cost[0], cost[1], cost[2]);
+    }
+    else if (n == 4)
+    {
+        ans = costofFourNodes(cost[0], cost[1], cost[2], cost[3]);
+    }
+    else
+    {
+        printf("The number of nodes is not supported by the program\n");
+        return 0;
+    }
     printf("The total cost of the network is: %f\n", ans);
 
     return 0;
