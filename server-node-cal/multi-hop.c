ff47ccccb4ca1e491909f4aa65eb001336b2fe9c
dhruvchandak30 <dhruvchandak5@gmail.com>
2024-10-24 15:25:25 +0530

    Added Float Inputs

--- server-node-cal/multi-hop.c
+++ server-node-cal/multi-hop.c
@@ -7,24 +7,47 @@ int min(int a, int b)
     return (a < b) ? a : b;
 }
 
-float findCostForOneElement(int cost[2])
+float findCostForOneElement(float cost[2])
 {
     return cost[0] + cost[1];
 }
 
-float findCostForTwoElements(int cost1[2], int cost2[2])
+float findCostForTwoElements(float cost1[2], float cost2[2])
 {
-    int c1 = cost1[0];
-    int c2 = cost2[0];
-    int c3 = cost1[1];
-    int c4 = cost2[1];
+    float c1 = cost1[0];
+    float c2 = cost2[0];
+    float c3 = cost1[1];
+    float c4 = cost2[1];
     return ((
-                ((1 - (1.0 / c1)) * (1.0 / c2) * (1 + c4)) + ((1.0 / c1) * ((1 - (1.0 / c2)) * (1 + c3))) +
+                ((1 - (1.0 / c1)) * (1 - (1.0 / c2))) +
+                ((1 - (1.0 / c1)) * (1.0 / c2) * (1 + c4)) +
+                ((1.0 / c1) * (1 - (1.0 / c2)) * (1 + c3)) +
                 ((1.0 / c1) * (1.0 / c2) * (1 + min(c3, c4)))) /
             ((1 - (1.0 / c1)) * (1 - (1.0 / c2))));
 }
 
-void storeCombinations(int arr[][2], int n, int **combinations, int *sizes, int *totalCombinations)
+float findCostForThreeElements(float cost1[2], float cost2[2], float cost3[2])
+{
+    float c1 = cost1[0];
+    float c2 = cost2[0];
+    float c3 = cost3[0];
+    float c4 = cost1[1];
+    float c5 = cost2[1];
+    float c6 = cost3[1];
+
+    return (((
+                 ((1 - (1.0 / c1)) * (1 - (1.0 / c2)) * (1 - (1.0 / c3))) +
+                 ((1 - (1.0 / c1)) * (1 - (1.0 / c2)) * (1.0 / c3) * (1 + c6)) +
+                 ((1 - (1.0 / c1)) * (1.0 / c2) * (1 - (1.0 / c3)) * (1 + c5)) +
+                 ((1 - (1.0 / c1)) * (1.0 / c2) * (1.0 / c3) * (1 + min(c5, c6))) +
+                 ((1.0 / c1) * (1 - (1.0 / c2)) * (1 - (1.0 / c3)) * (1 + c4)) +
+                 ((1.0 / c1) * (1 - (1.0 / c2)) * (1.0 / c3) * (1 + min(c4, c6))) +
+                 ((1.0 / c1) * (1.0 / c2) * (1 - (1.0 / c3)) * (1 + min(c4, c5))) +
+                 ((1.0 / c1) * (1.0 / c2) * (1.0 / c3) * (1 + min(min(c4, c5), c6)))) /
+             ((1 - (1.0 / c1)) * (1 - (1.0 / c2)) * (1 - (1.0 / c3)))));
+}
+
+void storeCombinations(float arr[][2], int n, int **combinations, int *sizes, int *totalCombinations)
 {
     int count = 0;
     int totalComb = (1 << n);
@@ -46,18 +69,22 @@ void storeCombinations(int arr[][2], int n, int **combinations, int *sizes, int 
     *totalCombinations = count;
 }
 
-void processCombinations(int **combinations, int *sizes, int totalCombinations, int arr[][2])
+void processCombinations(int **combinations, int *sizes, int totalCombinations, float arr[][2])
 {
     float minimum = FLT_MAX;
+    int bestCombinationIndex = -1;
+
     for (int i = 0; i < totalCombinations; i++)
     {
         if (sizes[i] == 1)
         {
             int node = combinations[i][0];
             float val = findCostForOneElement(arr[node - 1]);
+            printf("Cost for node %d is: %f\n", node, val);
             if (val < minimum)
             {
                 minimum = val;
+                bestCombinationIndex = i;
             }
         }
         else if (sizes[i] == 2)
@@ -65,13 +92,35 @@ void processCombinations(int **combinations, int *sizes, int totalCombinations, 
             int node1 = combinations[i][0];
             int node2 = combinations[i][1];
             float val = findCostForTwoElements(arr[node1 - 1], arr[node2 - 1]);
+            printf("Cost for nodes %d and %d is: %f\n", node1, node2, val);
             if (val < minimum)
             {
                 minimum = val;
+                bestCombinationIndex = i;
+            }
+        }
+        else if (sizes[i] == 3)
+        {
+            int node1 = combinations[i][0];
+            int node2 = combinations[i][1];
+            int node3 = combinations[i][2];
+            float val = findCostForThreeElements(arr[node1 - 1], arr[node2 - 1], arr[node3 - 1]);
+            printf("Cost for nodes %d, %d and %d is: %f\n", node1, node2, node3, val);
+            if (val < minimum)
+            {
+                minimum = val;
+                bestCombinationIndex = i;
             }
         }
     }
+
     printf("\n\nMinimum cost is: %f\n", minimum);
+    printf("Best combination is: { ");
+    for (int j = 0; j < sizes[bestCombinationIndex]; j++)
+    {
+        printf("Node %d ", combinations[bestCombinationIndex][j]);
+    }
+    printf("}\n");
 }
 
 int main()
@@ -89,14 +138,15 @@ int main()
     int *sizes = (int *)malloc((1 << n) * sizeof(int));
     int totalCombinations = 0;
 
-    int arr[n][2];
+    float arr[n][2];
     for (int i = 0; i < n; i++)
     {
         printf("Enter cost from server to node %d: ", i + 1);
-        scanf("%d", &arr[i][0]);
+        scanf("%f", &arr[i][0]);
         printf("Enter cost from node %d to client: ", i + 1);
-        scanf("%d", &arr[i][1]);
+        scanf("%f", &arr[i][1]);
     }
+    printf("\n\n");
 
     storeCombinations(arr, n, combinations, sizes, &totalCombinations);
 
