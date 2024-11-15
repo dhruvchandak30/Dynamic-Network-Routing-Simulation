800cd250e826d4021ae337b4ecd1c5bfbe7cba32
dhruvchandak30 <dhruvchandak5@gmail.com>
2024-11-15 23:52:04 +0530

    Added Calculations for Path via 1 Node

--- server-node-cal/multi-node.cpp
+++ server-node-cal/multi-node.cpp
@@ -10,11 +10,11 @@ using namespace std;
 #include <iostream>
 #include <vector>
 
-void justPrintTable( const vector<vector<int>> &a)
+void justPrintTable(const vector<vector<int>> &a)
 {
     for (int i = 0; i < a.size(); i++)
     {
-        cout << "Node " << i << ": ";
+        cout << "Path " << i + 1 << ": ";
         for (int j = 0; j < a[i].size(); j++)
         {
             cout << a[i][j] << " ";
@@ -85,10 +85,11 @@ struct Table
 {
     vector<int> combinations;
     float cost;
+    int NodeVal;
     vector<int> destinations;
 };
 
-void generateTableForMiddleNodes(int m, int n, vector<vector<int>> &a)
+vector<vector<Table>> generateTableForMiddleNodes(int m, int n, vector<vector<int>> &a)
 {
     // Create an ofstream object to write to a file
     std::ofstream outFile("output.txt");
@@ -96,10 +97,10 @@ void generateTableForMiddleNodes(int m, int n, vector<vector<int>> &a)
     if (!outFile)
     {
         std::cerr << "Error opening the file!" << std::endl;
-        return;
+        return std::vector<vector<Table>>();
     }
 
-    vector<Table> t;
+    vector<vector<Table>> t;
     int destNodes = n - m - 1;
     vector<vector<int>> combinations = generateBinaryCombinations(destNodes);
 
@@ -108,7 +109,7 @@ void generateTableForMiddleNodes(int m, int n, vector<vector<int>> &a)
         std::string header = "***    FOR NODE " + std::to_string(k) + "   ***\n\n";
         std::cout << header;
         outFile << header; // Write to file as well
-
+        vector<Table> tables;
         for (int i = 0; i < combinations.size(); i++)
         {
             vector<int> destinations;
@@ -134,7 +135,7 @@ void generateTableForMiddleNodes(int m, int n, vector<vector<int>> &a)
             Table entry;
             entry.combinations = combinations[i];
             entry.destinations = destinations;
-
+            entry.NodeVal = k;
             if (flag)
             {
                 entry.cost = -1;
@@ -155,7 +156,7 @@ void generateTableForMiddleNodes(int m, int n, vector<vector<int>> &a)
                 }
             }
 
-            t.push_back(entry);
+            tables.push_back(entry);
             if (zeroes != destNodes)
             {
                 // Printing to console
@@ -165,7 +166,7 @@ void generateTableForMiddleNodes(int m, int n, vector<vector<int>> &a)
                 }
                 std::cout << " -> Cost: " << entry.cost << std::endl;
 
-                std::cout << "Via Nodes: ";
+                std::cout << "To Nodes: ";
                 for (int i = 0; i < destinations.size(); i++)
                 {
                     std::cout << destinations[i];
@@ -184,7 +185,7 @@ void generateTableForMiddleNodes(int m, int n, vector<vector<int>> &a)
                 }
                 outFile << " -> Cost: " << entry.cost << std::endl;
 
-                outFile << "Via Nodes: ";
+                outFile << "To Nodes: ";
                 for (int i = 0; i < destinations.size(); i++)
                 {
                     outFile << destinations[i];
@@ -198,13 +199,15 @@ void generateTableForMiddleNodes(int m, int n, vector<vector<int>> &a)
             }
         }
         std::cout << std::endl;
-        outFile << std::endl; // Newline in the file
+        outFile << std::endl;
+        t.push_back(tables);
     }
-    outFile.close(); // Don't forget to close the file
+    outFile.close();
     std::cout << "Data written to output.txt" << std::endl;
+    return t;
 }
 
-vector<vector<int>> generateCombinations(int m)
+vector<vector<int>> generateCombinations(int m, int n, vector<vector<int>> &a)
 {
     vector<vector<int>> result;
     int totalCombinations = (1 << m);
@@ -224,7 +227,141 @@ vector<vector<int>> generateCombinations(int m)
         result.push_back(combination);
     }
 
+    for (int i = 0; i < result.size(); i++)
+    {
+        int flag = 0;
+        int count = 0;
+        vector<int> temp((n - (m + 1)), 0);
+        for (int j = 0; j < result[i].size(); j++)
+        {
+            int val = result[i][j];
+
+            for (int k = m + 1; k < n; k++)
+            {
+                if (a[val][k] == 0 && temp[k - m - 1] == 0)
+                {
+                    temp[k - m - 1] = 0;
+                }
+                else
+                {
+                    temp[k - m - 1] = 1;
+                }
+            }
+        }
+        for (int j = 0; j < temp.size(); j++)
+        {
+            if (temp[j] == 0)
+            {
+                flag = 1;
+            }
+        }
+        cout << endl;
+
+        if (flag)
+        {
+            result.erase(result.begin() + i);
+        }
+    }
+
     return result;
+}
+
+void findMinimumCost(int m, int n, vector<vector<int>> &a, vector<vector<int>> &combinations, vector<vector<Table>> tables)
+{
+    float minCost = 1000.00;
+    vector<int> minPath;
+    cout << "\n\n";
+    for (int i = 0; i < combinations.size(); i++)
+    {
+        float cost = 0;
+        vector<int> path;
+        path.push_back(0);
+        if (combinations[i].size() == 1)
+        {
+            int node = combinations[i][0];
+            for (int j = 0; j < tables[combinations[i][0]].size(); j++)
+            {
+                if (tables[node - 1][j].destinations.size() == (n - (m + 1)))
+                {
+                    cost += tables[node - 1][j].cost;
+                    path.push_back(tables[node - 1][j].NodeVal);
+                    for (int k = 0; k < tables[node - 1][j].destinations.size(); k++)
+                    {
+                        path.push_back(tables[node - 1][j].destinations[k]);
+                    }
+                }
+            }
+            cost += a[0][node];
+            cout << "Cost via 1 Node " << cost << endl;
+        }
+        if (combinations[i].size() == 2)
+        {
+            int node1 = combinations[i][0];
+            int node2 = combinations[i][1];
+            for (int j = 0; j < tables[node1 - 1].size(); j++)
+            {
+                for (int k = 0; k < tables[node2 - 1].size(); k++)
+                {
+                    if (tables[node1 - 1][j].destinations.size() == 1 && tables[node2 - 1][k].destinations.size() == 1)
+                    {
+                        if (tables[node1 - 1][j].destinations[0] == tables[node2 - 1][k].destinations[0])
+                        {
+                            cost += tables[node1 - 1][j].cost;
+                            path.push_back(tables[node1 - 1][j].NodeVal);
+                            path.push_back(tables[node1 - 1][j].destinations[0]);
+                            cost += a[0][node1];
+                            cost += a[node1][node2];
+                            cost += a[node2][tables[node1 - 1][j].destinations[0]];
+                            path.push_back(tables[node1 - 1][j].destinations[0]);
+                        }
+                    }
+                }
+            }
+            cout << "Cost via 2 Nodes " << cost << endl;
+        }
+        if (combinations[i].size() == 3)
+        {
+            int node1 = combinations[i][0];
+            int node2 = combinations[i][1];
+            int node3 = combinations[i][2];
+            for (int j = 0; j < tables[node1 - 1].size(); j++)
+            {
+                for (int k = 0; k < tables[node2 - 1].size(); k++)
+                {
+                    for (int l = 0; l < tables[node3 - 1].size(); l++)
+                    {
+                        if (tables[node1 - 1][j].destinations.size() == 1 && tables[node2 - 1][k].destinations.size() == 1 && tables[node3 - 1][l].destinations.size() == 1)
+                        {
+                            if (tables[node1 - 1][j].destinations[0] == tables[node2 - 1][k].destinations[0] && tables[node2 - 1][k].destinations[0] == tables[node3 - 1][l].destinations[0])
+                            {
+                                cost += tables[node1 - 1][j].cost;
+                                path.push_back(tables[node1 - 1][j].NodeVal);
+                                path.push_back(tables[node1 - 1][j].destinations[0]);
+                                cost += a[0][node1];
+                                cost += a[node1][node2];
+                                cost += a[node2][node3];
+                                cost += a[node3][tables[node1 - 1][j].destinations[0]];
+                                path.push_back(tables[node1 - 1][j].destinations[0]);
+                            }
+                        }
+                    }
+                }
+            }
+            cout << "Cost via 3 Nodes:  " << cost << endl;
+        }
+        if (cost < minCost)
+        {
+            minCost = cost;
+            minPath = path;
+        }
+    }
+    cout << "Minimum Cost: " << minCost << endl;
+    cout << "Path: ";
+    for (int i = 0; i < minPath.size(); i++)
+    {
+        cout << minPath[i] << " ";
+    }
+    cout << endl;
 }
 
 int main()
@@ -266,10 +403,11 @@ int main()
             a[i][j] = val;
         }
     }
-    cout << endl;
-    // generateTableForMiddleNodes(m, n, a);
-    vector<vector<int>> combinations = generateCombinations(m);
+    vector<vector<Table>> tables = generateTableForMiddleNodes(m, n, a);
+    vector<vector<int>> combinations = generateCombinations(m, n, a);
+    cout << "Nodes thorugh which the path can be established: \n";
     justPrintTable(combinations);
+    findMinimumCost(m, n, a, combinations, tables);
 
     // printTable(n, a);
 
